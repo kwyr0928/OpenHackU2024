b79a1540fd78ae339399fc32146fb55b6e6e4fd1
itoribero <nitopro2toro@gmail.com>
2024-11-10 23:43:58 +0900

    update:ログイン画面をfigmaを元に修正

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -4,23 +4,39 @@ import { Button } from "~/components/ui/button";
 
 export default function Login() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center text-center">
+    <div className="flex h-screen flex-col justify-center text-center font-mPlus bg-slate-50">
+
       {/* 新規登録 */}
-      <Button
-        onClick={() => signIn("google", { callbackUrl: "/question" })}
-        size="xl"
-        className="mb-10"
-      >
-        新規登録
-      </Button>
+      <div className="items-center">
+        <h1 className="text-darkBlue text-4xl">新規登録</h1>
+        <h2 className="text-darkBlue m-3 pb-7 text-base">初めての方は、こちらからGoogleアカウントを<br></br>使用してログインしてください</h2>
+        <Button
+          onClick={() => signIn("google", { callbackUrl: "/question" })}
+          size="xl"
+          className="w-80 h-20 bg-transparent hover:bg-blue-500 text-black text-2xl hover:text-white py-2 px-4 border-2 border-darkBlue hover:border-transparent rounded-3xl"
+        >
+          {/* Googleのロゴの取得先はこれで大丈夫？ */}
+          <img src="https://upload.wikimedia.org/wikipedia/commons/c/c1/Google_%22G%22_logo.svg" alt="google logo" />
+          Googleで登録
+        </Button>
+      </div>
+
+      <hr className="w-10/12 h-1 mx-auto m-16 bg-darkBlue border-0 rounded md:my-10 dark:bg-gray-700"></hr>
 
       {/* ログイン */}
-      <Button
-        onClick={() => signIn("google", { callbackUrl: "/home" })}
-        size="xl"
-      >
-        ログイン
-      </Button>
+      <div className="items-center">
+        <h1 className="text-darkBlue text-4xl pt-4">ログイン</h1>
+        <h2 className="text-darkBlue m-3 pb-7">既に登録済みの方は、こちらからGoogleアカウントを<br></br>使用してログインしてください</h2>
+        <Button
+          onClick={() => signIn("google", { callbackUrl: "/home" })}
+          size="xl"
+          className="w-80 h-20 bg-transparent hover:bg-blue-500 text-black text-2xl hover:text-white py-2 px-4 border-2 border-darkBlue hover:border-transparent rounded-3xl"
+        >
+          {/* Googleのロゴの取得先はこれで大丈夫？ */}
+          <img src="https://upload.wikimedia.org/wikipedia/commons/c/c1/Google_%22G%22_logo.svg" alt="google logo" />
+          Googleでログイン
+        </Button>
+      </div>
     </div>
   );
 }
