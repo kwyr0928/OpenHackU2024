1c0b870029c08e13d5e4875c005e3ae88143d013
uyuuuuu <soufflesumire.03@gmail.com>
2024-11-10 22:06:58 +0900

    remove: 削除

--- prisma/scripts/customQuery.sql
+++
@@ -1,8 +0,0 @@
--- scripts/custom-query.sql
-SELECT DISTINCT ON ("masterId")
-  *
-FROM "Items"
-WHERE
-  "userId" = 'cm39uc7uv0003btvndmmqt346' AND
-  "itemType" = 2
-ORDER BY "masterId", "created_at" DESC;
