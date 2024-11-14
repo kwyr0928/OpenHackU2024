373848e1dd2f472b196c8a4086494e23aef7b2c1
uyuuuuu <soufflesumire.03@gmail.com>
2024-11-15 05:16:35 +0900

    fix: getKindItemsでスケジュールアイテムを含めない

--- src/server/repositry/getdata.ts
+++ src/server/repositry/getdata.ts
@@ -309,6 +309,7 @@ export async function getKindItems(userId: string, type: number) {
       where: {
         parentId: null,
         itemType: type,
+        isSetting: false
       },
       orderBy: {
         created_at: "asc",
